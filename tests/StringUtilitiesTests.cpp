11ecef76557ab05e02aa4b7dfd956353b63fcb95
Juan Carlos Arevalo Baeza <jcab@JCABs-Rumblings.com>
2026-07-14 18:17:33 -0700

    Fix the string tests to use the test utilities library

--- tests/StringUtilitiesTests.cpp
+++ tests/StringUtilitiesTests.cpp
@@ -1,10 +1,10 @@
 
 #include "StringUtilities.h"
 
+#include "TestUtilities.h"
+
 #include <iostream>
 #include <string>
-
-#include <assert.h>
 
 int main()
 {
@@ -20,23 +20,23 @@ int main()
     std::string original  = "Hello \"World\"! \n New line.";
     std::string escaped   = EscapeString(original);
     std::string unescaped = UnescapeString(escaped);
-    assert(original == unescaped);
+    ExpectEqual(original, unescaped, "expected unescaping recovers an escaped string");
 
     std::cout << "Test OneLine\n";
     std::string multiLine = "Line 1\nLine 2\nLine 3";
     std::string oneLine   = OneLine(multiLine, 10);
     // It should be at most 10 characters long.
-    assert(oneLine.length() <= 10 + 1 + 3); // Adds the "\n" and "...".
+    Expect(oneLine.length() <= 10 + 1 + 3, "OneLine must return a size-limited string"); // Adds the "\n" and "...".
 
     std::cout << "Test ReplaceNewlinesIn\n";
-    std::string nlString = "Line 1\nLine 2\nLine 3";
+    std::string nlString = "Line 1\nLine 2\r\nLine 3";
     ReplaceNewlinesIn(nlString, " ");
-    assert(nlString == "Line 1 Line 2 Line 3");
+    ExpectEqual(nlString, "Line 1 Line 2 Line 3", "ReplaceNewlinesIn must find and replace all newlines");
 
     std::cout << "Test ReplaceIn\n";
     std::string replaceStr = "The quick brown fox";
     ReplaceIn(replaceStr, "brown", "red");
-    assert(replaceStr == "The quick red fox");
+    ExpectEqual(replaceStr, "The quick red fox", "ReplaceIn must replace the given string");
 
     // Test ConfigureConsoleForUtf8 (hard to test without a real console, but we can check it doesn't crash)
     ConfigureConsoleForUtf8();
